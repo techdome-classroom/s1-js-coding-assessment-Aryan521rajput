1a7a7c2a7e3ede47cc58d24ec31b452dab0101d0
Aryan521 <rajputaryan521@gmail.com>
2024-10-07 12:21:07 +0530

    Program1 done

--- program1.js
+++ program1.js
@@ -1,8 +1,38 @@
 const getTotalIsles = function (grid) {
+  
+  if (!grid || grid.length === 0) return 0;
 
+  
+  const dfs = (grid, i, j) => {
+      
+      if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 'W') {
+          return;
+      }
+      
 
-  // write your code here
+      grid[i][j] = 'W';
 
+      
+      dfs(grid, i - 1, j); 
+      dfs(grid, i + 1, j); 
+      dfs(grid, i, j - 1); 
+      dfs(grid, i, j + 1);
+  };
+
+  let islandCount = 0;
+
+  
+  for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+          
+          if (grid[i][j] === 'L') {
+              islandCount++;  
+              dfs(grid, i, j);  
+          }
+      }
+  }
+
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
